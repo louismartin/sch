5fc201eb03ffcff142e3d23f63a003dfc29e85d3
qmoret <qmoretdero@gmail.com>
2017-02-14 15:27:13 +0100

    Nouveaux plots

--- td1/code/td1.R
+++ td1/code/td1.R
@@ -6,4 +6,16 @@ df[df==""] <- NA
 df <- na.omit(df)
 
 # Plot TER
-plot(seq(1:length(df$PER)), df$PER)
+
+
+# Plot PER & TX10
+ymax <- max(max(df$PER),max(df$TX10))
+plot(c(1:nrow(df)), df$PER, type = 'l', ylim = c(0, ymax), col = 'red', 
+     main = "Evolution de PER et TX10 en fonction du temps",
+     ylab = '',
+     xlab = 'Index temporel')
+lines(c(1:nrow(df)), df$TX10, ylim = c(0, ymax), col = 'blue')
+legend("topright",legend = c('PER', 'TX10'), col = c('red', 'blue'), lwd = c(1,1))
+
+# Plot PER / TX10
+plot(df$TX10, df$PER, xlab = 'TX10', ylab = 'PER')
